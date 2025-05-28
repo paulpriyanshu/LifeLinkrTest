3c0a77bac2c9678647a2c3b90d0d9ee2f42afc4e
paulpriyanshu <priyanshu.paul003@gmail.com>
2025-05-28 12:53:15 +0530

    newchanges4

--- api/routes/UploadRoutes.js
+++ api/routes/UploadRoutes.js
@@ -32,7 +32,7 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const upload = multer({ storage, fileFilter });
+const upload = multer({ storage, fileFilter , limits: { fileSize: 5 * 1024 * 1024 } });
 
 router.post("/upload", upload.single("file"), (req, res) => {
   if (!req.file) {
