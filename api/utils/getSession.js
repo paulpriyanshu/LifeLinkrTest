a7f5c22676562afb27938403b336cf81ac648f9b
paulpriyanshu <priyanshu.paul003@gmail.com>
2025-05-28 11:40:37 +0530

    init8

--- api/utils/getSession.js
+++ api/utils/getSession.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 export const getSession=(req,res)=>{
     console.log("entered the function")
-    console.log(req.cookies)
+    console.log(JSON.stringify(req.cookies))
     const token=req.cookies["auth-token"]
       if (!token) return res.status(401).json({ user: null });
     try {
